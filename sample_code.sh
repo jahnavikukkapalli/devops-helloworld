e3d22b507a5c200f273001efaade10f1c48abb90
jahnavikukkapalli <140580697+jahnavikukkapalli@users.noreply.github.com>
2023-08-30 10:55:23 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -3,7 +3,7 @@
 
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
-echo "This is an example of new script"
+echo "This is an example of net script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
